1e85720b19432a3495aafb657c2b9fff46e9e039
Jacob Nyyssønen Henriksen <jaco9515@zbc.dk>
2023-08-15 08:40:17 +0200

    Removed log features

--- BananaKeep-SecurityCentral/Program.cs
+++ BananaKeep-SecurityCentral/Program.cs
@@ -7,22 +7,10 @@ builder.Services.AddControllers();
 var app = builder.Build();
 app.MapControllers(); // Map controllers from GPSController and other controllers
 
-
 //Initialize the database
 DummyDatabase.Initialize();
 
-
-
-//Rewrite console.writeline to log to file
-
-var logFilePath = "console.log"; // Path to the log file
-var logFileStream = new FileStream(logFilePath, FileMode.Append);
-var logStreamWriter = new StreamWriter(logFileStream) { AutoFlush = true };
-
-Console.SetOut(logStreamWriter);
-
 app.Run();
-
 
 Console.WriteLine("Hello World!");
 Console.WriteLine("Testing MVC LogView");
